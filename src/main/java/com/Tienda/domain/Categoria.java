a68f888acbff989ea40ec413490473f565bf75e7
MAGIC <MAGIC@DESKTOP-AI8AH5Q>
2024-07-30 18:08:21 -0600

    semana 9

--- src/main/java/com/Tienda/domain/Categoria.java
+++ src/main/java/com/Tienda/domain/Categoria.java
@@ -1,27 +1,30 @@
-
 package com.Tienda.domain;
 
-import jakarta.persistence.Column;
+import jakarta.persistence.*;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+
 import jakarta.persistence.Table;
 import java.io.Serializable;
+import java.util.List;
 import lombok.Data;
+
 @Data
 @Entity
-@Table(name="categoria")
-public class Categoria implements Serializable{
+@Table(name = "categoria")
+public class Categoria implements Serializable {
+
     private static final long serialVersionUID = 1L;
-    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id_categoria")
+    @Column(name="id_categoria")
     private Long idCategoria;
     private String descripcion;
     private String rutaImagen;
     private boolean activo;
+    
+    @OneToMany
+    @JoinColumn(name = "id_categoria", insertable = false, updatable = false)
+    List<Producto> productos;
 
     public Categoria() {
     }
@@ -31,7 +34,5 @@ public class Categoria implements Serializable{
         this.rutaImagen = rutaImagen;
         this.activo = activo;
     }
-    
-    
-    
+
 }
